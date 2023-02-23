055a75e2c980c5ccde8351838632c8e2e9c47d94
JuanGomez-jpg <juan.mojarro7920@alumnos.udg.mx>
2023-02-23 16:16:13 -0600

    migrations done

--- database/migrations/2023_02_23_220920_create_albums_table.php
+++ database/migrations/2023_02_23_220920_create_albums_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('albums', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('birthday', 10);
-            $table->rememberToken();
+            $table->string('albumName', 30);
+            $table->string('year', 10);
+            $table->string('genre', 25);
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('albums');
     }
 };
