1cf2b852e4be652e29c831db2251200a5d1d0483
JuanGomez-jpg <juan.mojarro7920@alumnos.udg.mx>
2023-03-30 15:07:25 -0600

    album cards fixed

--- resources/views/albums/album.blade.php
+++ resources/views/albums/album.blade.php
@@ -5,9 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--<link rel="stylesheet" type="text/css" href="{{ asset('css/albums.css') }}">-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/albums.css') }}">
     <link href="{{ asset('css/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/bootstrap/bootstrap.min.js') }}"></script>
     <title>Albums</title>
 </head>
 <body>
@@ -30,25 +31,51 @@
         @endforeach-->
     <!--</div>-->
 
-  <div class="row">
-    @foreach ($albums as $al)
-      <div class="col-3">
-        <div class="card-deck">
-          <div class="card shadow p-3 mb-5 bg-white rounded" style="height: 20rem; width:20rem;">
-            <div class="text-center">
-              <img src="{{ asset('storage/images/'.$al->coverImg) }}" class="img-fluid" style="max-width: 40%; height: auto;" />
-              <div class="card-body">
-                <h5 class="card-title">{{ $al -> albumName }}</h5>
-                <p class="card-text">{{ $al -> artistName }}</p>
-                <a href="/albums/{{ $al->id }}" class="btn btn-outline-primary">Ver</a>
-                <a href="/albums/{{ $al->id }}/edit" class="btn btn-outline-warning">Editar</a>
-              </div>
-            </div>
+    <!--
+<div class="row flex-wrap">
+  @foreach ($albums as $al)
+    <div class="col-md-3">
+      <div class="card shadow p-3 mb-5 bg-white rounded" style="height: 20rem; width:20rem;">
+        <div class="text-center">
+          <img src="{{ asset('storage/images/'.$al->coverImg) }}" class="img-fluid" style="max-width: 40%; height: auto;" />
+          <div class="card-body">
+            <h5 class="card-title">{{ $al -> albumName }}</h5>
+            <p class="card-text">{{ $al -> artistName }}</p>
+          </div>
+          <div class="card-footer bg-transparent">
+            <a href="/albums/{{ $al->id }}" class="btn btn-outline-primary">Ver</a>
+            <a href="/albums/{{ $al->id }}/edit" class="btn btn-outline-warning">Editar</a>
           </div>
         </div>
       </div>
-    @endforeach
-  </div>
+    </div>
+  @endforeach
+</div>
+-->
+<div class="album py-3">
+  <div class="container">
+    <div class="row">
+      @foreach ($albums as $al)
+        <div class="col-md-3">
+          <div class="card shadow p-3 mb-5 bg-white rounded">
+            <div class="text-center">
+              <img src="{{ asset('storage/images/'.$al->coverImg) }}" class="card-img-top" style="max-width: 40%; height: auto;" />
+                <div class="card-body" style="height: 7rem;">
+                  <h5 class="card-title">{{ $al -> albumName }}</h5>
+                  <p class="card-text">{{ $al -> artistName }}</p>
+                </div>
+                <div class="card-footer bg-transparent">
+                  <a href="/albums/{{ $al->id }}" class="btn btn-outline-primary">Ver</a>
+                  <a href="/albums/{{ $al->id }}/edit" class="btn btn-outline-warning">Editar</a>
+                </div>
+            </div>
+          </div>
+        </div>
+      @endforeach
+    </div>
+   </div>
+</div>
+
 
 
 
