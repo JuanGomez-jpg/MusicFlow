0361daa6ed1710992eb1203b3b93de7a2b69d3c4
JuanGomez-jpg <juan.mojarro7920@alumnos.udg.mx>
2023-03-30 23:09:50 -0600

    navbar working

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,40 +1,47 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Mi Sitio</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Inicio</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Acerca de</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Servicios</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contacto</a>
-      </li>
-    </ul>
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Botón 1</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Botón 2</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <img src="https://avatars.githubusercontent.com/u/83151926?s=400&u=71c9ba99762b69b2da340d66e3e7408d385d7c14&v=4" alt="Imagen de perfil" class="img-fluid" style="max-width: 2rem;">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <i class="bi bi-music-note-beamed"></i>
+            <span class="text-warning">MusicFlow</span>
         </a>
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Opción 1</a>
-          <a class="dropdown-item" href="#">Opción 2</a>
-          <a class="dropdown-item" href="#">Opción 3</a>
+
+        <button class="navbar-toggler" type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#menu"
+        aria-controls="navbarSupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="menu">
+            <ul class="navbar-nav me-auto">
+
+                <li class="nav-item">
+                    <a class="nav-link active" href="/albums">Inicio</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" 
+                        href="#" 
+                        id="navbarDropdown" 
+                        role="button" 
+                        data-bs-toggle="dropdown" 
+                        aria-expanded="false"
+                        >Cuenta
+                    </a>
+                    <ul class="dropdown-menu bg-secondary" aria-labelledby="navbarDropdown">
+                        <li>
+                            <a class="dropdown-item" href="">Cerrar sesión</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="">Ajustes</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <hr class="text-white-50">
+            
         </div>
-      </li>
-    </ul>
-  </div>
+
+    </div>
 </nav>
